7c4a89bc90d7e94425815d482bcf404af93975f8
Liv0130 <yjjhsh27@gmail.com>
2022-03-25 23:57:55 +0900

    Installed react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import {List} from 'react-virtualized';
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+    const todo = todos[index];
   return (
-    <div className="TodoList">
-      {todos.map(todo => (
-      <TodoListItem 
+    <TodoListItem
       todo={todo} 
-      key={todo.id} 
+      key={key} 
       onRemove={onRemove}
       onToggle={onToggle}
+      style={style}
       />
-      ))}
-    </div>
-  )
-}
+      )},
+      [onRemove, onToggle, todos],
+  );
+  return (
+    <List
+    className="TodoList"
+    width={512} // 전체크기
+    height={513} // 전체높이
+    rowCount={todos.length} // 항목개수
+    rowHeight={57} // 항목을 렌더링할 때 쓰는 함수
+    rowRenderer={rowRenderer}
+    list={todos} // 배열
+    style={{ outline:'none' }} // List에 기본적용되는 outline 스타일 제거
+    />
+  );
+};
 
 export default React.memo(TodoList);
